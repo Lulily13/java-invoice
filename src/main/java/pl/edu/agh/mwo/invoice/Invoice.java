ef4b2b8a339c26734c5eba715710d7ecd5b6edbe
Lulily13 <bloodyakuma13@yahoo.com>
2025-02-16 17:03:33 +0100

    Add implementation of Invoice methods

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,52 @@
 package pl.edu.agh.mwo.invoice;
 
-import java.math.BigDecimal;
-import java.util.Collection;
-
 import pl.edu.agh.mwo.invoice.product.Product;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Invoice {
-    private Collection<Product> products;
+    private Map<Product, Integer> productsWithQuantities = new HashMap<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("Products cannot be null");
+        }
+        if (quantity == null || quantity <= 0) {
+            throw new IllegalArgumentException("Quantity of products must be greater than zero");
+        }
+
+        productsWithQuantities.put(product, productsWithQuantities.getOrDefault(product, 0) + quantity);
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetPrice() {
+        BigDecimal netPrice = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> entry : productsWithQuantities.entrySet()) {
+            netPrice = netPrice.add(entry.getKey().getPrice().multiply(BigDecimal.valueOf(entry.getValue())));
+        }
+        return netPrice;
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal totalTax = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> entry : productsWithQuantities.entrySet()) {
+            BigDecimal productTax = entry.getKey().getPrice()
+                    .multiply(entry.getKey().getTaxPercent())
+                    .multiply(BigDecimal.valueOf(entry.getValue()));
+            totalTax = totalTax.add(productTax);
+        }
+        return totalTax;
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossPrice() {
+        return getNetPrice().add(getTax());
     }
 }
